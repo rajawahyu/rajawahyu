108f0b1fbd36f2f2a9f319b41fb8e05719fa1d98
rajawahyu <wahyukaze404@gmail.com>
2025-06-13 11:30:19 +0700

    Add files via upload

---
+++ hrl.php
@@ -0,0 +1,16 @@
+<?php
+//database = film //
+// tabel = film  //
+//id_film  //
+//judul_film //
+// genre //
+//sutradara	//
+// tahun_rilis //
+
+// tabel kedua dibawah//
+// tabel = users //
+// id_user //
+// username //
+// password //
+// role //
+?>
